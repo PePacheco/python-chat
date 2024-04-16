497b77ef4a2e3247edcb78acf284ffec8add4346
PePacheco <pedro_pacheco98@outlook.com>
2024-04-16 19:52:59 -0300

    fixed conflicts

--- clienttcp.py
+++ clienttcp.py
@@ -1,5 +1,6 @@
 import sys
 import threading
+import os
 import socket
 from Util.msgfiltered import Msgfiltered
 
@@ -7,25 +8,31 @@ serverName = '127.0.0.1'
 serverPort = 12345
 msg_filter = Msgfiltered()
 
-def enviar_dados(client_socket, message):
+def send_data(client_socket, message):
     while True:
         # Aguarda entrada do usuário
-        entrada = input("Digite uma mensagem para enviar (ou EXIT para sair): ")
-        if entrada.upper() == "EXIT":
+        entry = input("Digite uma mensagem para enviar (ou EXIT para sair): ")
+        if entry.upper() == "EXIT":
             break
-        client_socket.send(entrada.encode())
+        message = entry.split(' ')[1]
+        abs_path = os.path.abspath(message)
+        if os.path.isfile(abs_path):
+            with open(abs_path, 'rb') as file:
+                file_data = file.read()
+                print(file_data)
+                message_to_send = f"{entry} {len(file_data)}".encode() + file_data
+                client_socket.sendall(message_to_send)
+            client_socket.send(entry.encode())
+        else:
+            client_socket.send(entry.encode())
 
-def receber_dados(client_socket):
+def receive_data(client_socket):
     while True:
         # Recebe dados do servidor
         message = client_socket.recv(1024)
         if message:
-<<<<<<< HEAD
             msg = msg_filter.process_message(message.decode())
             print(f"{msg.param1}: {msg.param2}")
-=======
-            print(message.decode())
->>>>>>> origin/Pedrao
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
@@ -39,10 +46,8 @@ if msg.param1 != "\\REG" and msg.param3 != None:
 
 clientSocket.send(message.encode())
 
-enviar_thread = threading.Thread(target=enviar_dados, args=(clientSocket, None))
-receber_thread = threading.Thread(target=receber_dados, args=(clientSocket,))
+enviar_thread = threading.Thread(target=send_data, args=(clientSocket, None))
+receber_thread = threading.Thread(target=receive_data, args=(clientSocket,))
 
 enviar_thread.start()
 receber_thread.start()
-
-
